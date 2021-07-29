b847ca17739985a87757468af0d14e99132f8d7e
Neha121995 <jagtapneha1212@gmail.com>
2021-07-29 21:27:34 +0530

    [Neha] Added: User Registartion Exception Handing For mobile number

--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ namespace UserRegistration_ExceptionHandling
             Console.WriteLine("Welcome to User Registartion Problem Using Exception Handling");
             UserRegisterPattern UserRegisterPattern = new UserRegisterPattern();
 
-            Console.Write("Enter the valid email: ");
+            Console.Write("Enter the valid Mobile Number: ");
             string Email = Console.ReadLine();
-            Console.WriteLine("The test result :: " + UserRegisterPattern.validateEmail(Email));
+            Console.WriteLine("The test result :: " + UserRegisterPattern.validateMobileNumber(Email));
 
         }
         
--- UserRegisterPattern.cs
+++ UserRegisterPattern.cs
@@ -7,14 +7,14 @@ namespace UserRegistration_ExceptionHandling
 {
     public class UserRegisterPattern
     {
-        public static String Regex_email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
-        public bool validateEmail(string email)
+        public static String Regex_mobileNumber = "(0|91)?[ ][6-9][0-9]{9}";
+        public bool validateMobileNumber(string mobileNo)
         {
-            if (email == null)
+            if (mobileNo == null)
             {
-                throw new UserRegistrationClass(UserRegistrationClass.ExceptionsType.Invalid_MESSAGE, "Entered Invalid Email");
+                throw new UserRegistrationClass(UserRegistrationClass.ExceptionsType.Invalid_MESSAGE, "Entered Invalid Mobile Number");
             }
-            return Regex.IsMatch(email, Regex_email);
+            return Regex.IsMatch(mobileNo, Regex_mobileNumber);
         }
 
     }
